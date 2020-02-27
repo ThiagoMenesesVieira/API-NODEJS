523ac8f37909a80d21fe4e3dfea5660f293b7f0c
ThiagoMenesesVieira <thiagomenesesvieira@gmail.com>
2020-02-27 18:40:27 -0300

    cors inclusion

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const consign = require('consign')
 const bodyParser = require('body-parser')
 
@@ -6,6 +7,7 @@ module.exports = () => {
     const app = express()
     app.use(bodyParser.urlencoded({extended: true}))
     app.use(bodyParser.json())
+    app.use(cors())
     consign()
         .include('controllers')
         .into(app)
